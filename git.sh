b3bbf253e38ba78457f9c45af27c3621c3426717
lucas55412 <lucashsu6626@gmail.com>
2025-09-29 19:23:44 +0800

    自動同步更新 2025-09-29 19:23:44

--- git.sh
+++ git.sh
@@ -18,23 +18,3 @@ else
 
     echo "已同步到遠端 GitHub ✅"
 fi
-#!/bin/bash
-
-# 檢查是否有修改
-status=$(git status --porcelain)
-
-if [ -z "$status" ]; then
-    echo "沒有新的修改，工作區乾淨 ✅"
-else
-    # 暫存所有修改
-    git add .
-
-    # commit 訊息加上時間
-    datetime=$(date '+%Y-%m-%d %H:%M:%S')
-    git commit -m "自動同步更新 $datetime"
-
-    # 使用 SSH 自動推送到 GitHub
-    git push origin main
-
-    echo "已同步到遠端 GitHub ✅"
-fi
